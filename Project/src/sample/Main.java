9174046d0c9f4d42a33c7ddb80e4852c12abe445
Ahmed Ayman 2000128 <aa2712374@gmail.com>
2023-12-04 19:58:04 +0200

    start2

--- Project/src/sample/Main.java
+++ Project/src/sample/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //start of project
+	//start2
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
